0ebad2b8401be6942522ca420800bb828b59e5de
cong-tri <daocongtri20031609@gmail.com>
2023-02-10 20:19:46 +0700

    submit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-$(".slick-content").slick({
+$(".slick").slick({
   infinite: true,
   slidesToShow: 5,
-  autoplay: true,
-  autoplaySpeed: 4e3,
   speed: 1100,
   arrows: false,
   dots: true,
-  prevArrow:
-    '<button type="button" class="service-prev"><i class="fa fa-long-arrow-alt-left"></i></button>',
-  nextArrow:
-    '<button type="button" class="service-next"><i class="fa fa-long-arrow-alt-right"></i></button>',
 }),
   $('[data-fancybox="gallery"]').fancybox({ loop: true }),
   $(".counter").countUp();
+function checkForm(){
+      var email = document.getElementById("email").value;
+      var username = document.getElementById("username").value;
+      var feedback = document.getElementById("feedback").value;
+      if (!email || !username || !feedback) {
+        alert(
+          "Username, Email, Feedback must be required!"
+        );
+        return false;
+      }
+      alert("Congratulation! Your feedback information has been sent successfully!");
+      return true;
+}
+
